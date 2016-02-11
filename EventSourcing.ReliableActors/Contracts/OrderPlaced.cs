83cb4a9a40f24244ce02a3368d3f481f016eed87
Jaimie Avery <jaimie.avery@gmail.com>
2016-02-11 18:20:49 +0000

    Created web job for order commands

---
+++ EventSourcing.ReliableActors/Contracts/OrderPlaced.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Contracts
+{
+    public class OrderPlaced : IEvent
+    {
+        public OrderPlaced(string value)
+        {
+            Value = value;
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; }
+
+        public string Value { get; }
+    }
+}
